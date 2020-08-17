66490c24c6c05c9ed2e382ef74904f6bf248741d
Ben <benkatz670@gmail.com>
2020-08-16 17:02:35 -0700

    major ui changes

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-figma.ui.resize(800, 605);
+figma.ui.resize(680, 730);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
