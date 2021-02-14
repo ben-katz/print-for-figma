9f79c15dfe5ef726898a3ce6a2bd935c37146406
Ben Katz <benkatz670@gmail.com>
2021-02-13 21:12:49 -0800

    add bleed support, refactor UI

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-figma.ui.resize(680, 730);
+figma.ui.resize(442, 660);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
@@ -15,24 +15,59 @@ figma.ui.onmessage = preset => {
     const nodes = [];
     const dpi = preset.dpi;
     const dpmm = (preset.dpi / 2.54237288136) * 0.1;
+    const doc_name = preset.name != "" ? preset.name : 'My Document';
+    function clone(val) {
+        return JSON.parse(JSON.stringify(val));
+    }
     for (let i = 0; i < preset.pages; i++) {
         const frame = figma.createFrame();
-        var frameWidth = preset.width * dpmm;
-        var frameHeight = preset.height * dpmm;
+        var frameWidth = (preset.width * dpmm) + 2 * preset.bleed * dpmm;
+        var frameHeight = (preset.height * dpmm) + 2 * preset.bleed * dpmm;
         if (preset.unit == 'inches') {
-            frameWidth = preset.width * dpi;
-            frameHeight = preset.height * dpi;
+            frameWidth = (preset.width * dpi) + 2 * preset.bleed * dpi;
+            frameHeight = (preset.height * dpi) + 2 * preset.bleed * dpi;
         }
         frame.resizeWithoutConstraints(frameWidth, frameHeight);
         frame.x = (figma.viewport.center.x - frameWidth / 2) + (i * frameWidth * 1.10);
         frame.y = figma.viewport.center.y - frameHeight / 2;
         frame.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
-        frame.name = `Page ${i + 1} (${preset.name} @${dpi}dpi)`;
+        frame.name = `${doc_name} @${dpi}dpi (Page ${i + 1})`;
+        // Check if bleed is enabled:
+        // We already added the bleed to the total document size in the lines above, so we only need to add the child frame or not.
+        if (preset.bleed != 0) {
+            if (preset.unit == 'inches') {
+                var bleedPixels = preset.bleed * dpi;
+            }
+            else {
+                var bleedPixels = preset.bleed * dpmm;
+            }
+            var x1 = bleedPixels;
+            var y1 = bleedPixels;
+            var x2 = frameWidth - bleedPixels;
+            var y2 = frameHeight - bleedPixels;
+            frame.guides = [{ axis: 'X', offset: x1 }, { axis: 'X', offset: x2 }, { axis: 'Y', offset: y1 }, { axis: 'Y', offset: y2 }];
+            const bleedFrame = figma.createFrame();
+            bleedFrame.resizeWithoutConstraints(frameWidth - (bleedPixels * 2), frameHeight - (bleedPixels * 2));
+            bleedFrame.name = `Visible Area`;
+            //bleedFrame.strokes = [{type: 'SOLID', color: {r: 0.9, g: 0.1, b: 0.55}}];
+            //bleedFrame.strokeWeight = 0.5;
+            //bleedFrame.strokeAlign = "OUTSIDE"
+            bleedFrame.clipsContent = false;
+            frame.layoutMode = "VERTICAL";
+            frame.primaryAxisAlignItems = "CENTER";
+            frame.counterAxisAlignItems = "CENTER";
+            frame.paddingLeft = bleedPixels;
+            frame.paddingRight = bleedPixels;
+            frame.paddingTop = bleedPixels;
+            frame.paddingBottom = bleedPixels;
+            frame.appendChild(bleedFrame);
+        }
         figma.currentPage.appendChild(frame);
         nodes.unshift(frame);
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.notify('Document Created!');
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     //figma.closePlugin();
